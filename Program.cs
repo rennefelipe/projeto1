0c34ea5acdcad63f80a275a3b822d9b2d73a55a6
renne.fsgalindo <renne.fsgalindo@OSA0625241W10-1>
2022-08-01 21:21:22 -0300

    atualizaçao de nomeAdicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int Numero_Chamado = 0;
+            String Nome_Aluno = "Renne Fellipe 11";
+
+            Numero_Chamado = 1;
+            Console.WriteLine(Numero_Chamado);
+            Console.WriteLine(Nome_Aluno);
+
+            Console.WriteLine(args[0].ToString());
+            Console.WriteLine(args[1].ToString());
+            Console.WriteLine(args[2].ToString());
+            Console.WriteLine(args[3].ToString());
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
